3935f5ba4eba7118c7ed3e2ded0907521cdce1fa
Bharath Nair <bharath.rajeevnair@student.adelaide.edu.au>
2020-11-09 17:41:07 +1030

    Typing animation in the home page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,24 @@
 // import Head from 'next/head'
-// import styles from '../styles/Home.module.css'
+//import styles from '../styles/Home.module.css'
 import Layout from '../components/Layout';
 import MainButtons from '../components/MainButtons';
+import Typing from 'react-typing-animation';
+
+const AnimatedTypingComponent = () => (
+  <Typing>
+     <Typing speed={500} />
+    <span><h1>Welcome to Rate my Course</h1></span>
+    <Typing.Speed ms={50} />
+    <span><h2>An anonymous platform for students to inform other students before hand about a class they are going to take</h2></span>
+  </Typing>
+);
 
 export default function Home() {
   return (
     <Layout>
+      <AnimatedTypingComponent />
       <MainButtons />
     </Layout>
-   
+
   )
 }
